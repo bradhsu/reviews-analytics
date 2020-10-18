c1d441e024458ab75631bffbeac47a5e7b079cd6
Brad Hsu <1980bradhsu@gmail.com>
2020-10-18 21:37:04 +0800

    calculate average

--- read.py
+++ read.py
@@ -7,5 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-print(len(data))
-print(data[0])
+print('Finish reading', 'Total ', len(data), 'lines')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('The average length of the reviews is ', sum_len/len(data))
